362621ce22b3481634f03e957a24a117936e70d2
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-05-04 20:12:56 -0600

    add HomeUsuarioComponent and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { UsertypeComponent } from './pages/usertype/usertype.component';
 import { UserRegisterComponent } from './pages/user-register/user-register.component';
 import { GroupRegisterComponent } from './pages/group-register/group-register.component';
+import { HomeUsuarioComponent } from './pages/home-usuario/home-usuario.component';
 
 export const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'usertype', component: UsertypeComponent},
   {path: 'usertype/user', component: UserRegisterComponent},
   {path: 'usertype/group', component: GroupRegisterComponent},
+  {path: 'home', component: HomeUsuarioComponent},
 ];
