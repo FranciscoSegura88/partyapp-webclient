aa23e35469c55b1bec16246908ef5e1e5fd10984
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-05-04 20:52:44 -0600

    add LoginComponent and update routing for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { UsertypeComponent } from './pages/usertype/usertype.component';
 import { UserRegisterComponent } from './pages/user-register/user-register.component';
 import { GroupRegisterComponent } from './pages/group-register/group-register.component';
 import { HomeUsuarioComponent } from './pages/home-usuario/home-usuario.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {path: '', component: InicioComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'usertype', component: UsertypeComponent},
   {path: 'usertype/user', component: UserRegisterComponent},
   {path: 'usertype/group', component: GroupRegisterComponent},
