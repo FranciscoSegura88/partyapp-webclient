7c1ecd65150d6d95c646fb448e0566a2d6266fbd
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-05-14 00:24:54 -0600

    add chat functionality with message input and display

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+  mensajes: string[] = [];
+  nuevoMensaje: string = '';
 
+  enviarMensaje(chatBox: HTMLElement) {
+    const mensaje = this.nuevoMensaje.trim();
+    if (mensaje !== '') {
+      this.mensajes.push(mensaje);
+      this.nuevoMensaje = '';
+
+      // Hacer scroll al final
+      setTimeout(() => {
+        chatBox.scrollTop = chatBox.scrollHeight;
+      });
+    }
+  }
 }
